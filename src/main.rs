9b86db2d598d3088b8b216b867fea6dc4bfe6f6e
Will Larson <lethain@gmail.com>
2019-07-12 18:44:24 -0700

    Well, template rendering works to a non-zero extent, which is good

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
 use std::collections::HashMap;
+use std::path::Path;
+
+extern crate tera;
 
 #[derive(Debug)]
 struct Context {
@@ -11,6 +14,25 @@ impl Context {
         let hm = HashMap::new();
         Context{vars: hm}
     }
+
+    fn render_template(&self, path: String) -> String {
+        let path: &Path = Path::new(&path);
+        let dir = path.parent().unwrap().join("*").to_str().unwrap().to_string();
+        let filename = path.file_name().unwrap().to_str().unwrap();
+
+        let mut tera_ctx = tera::Context::new();
+        tera_ctx.insert("age", &18);
+
+        match tera::Tera::new(&dir) {
+            
+            Ok(t) => t.render(filename, &tera_ctx).unwrap().as_str().to_string(),
+            Err(e) => {
+                println!("[literal] Failed loading tempaltes: {}", e);
+                "".to_string()
+            }            
+        }
+    }
+
     fn process<'a>(&'a mut self, line: String) {
         let line = line.trim();
         if line.starts_with("\\") {
@@ -28,6 +50,11 @@ impl Context {
                         Some(current) => *current += value,
                         None => eprintln!("[literal] incremented non-existant variable: {}", line),
                     }
+                },
+                "\\render" => {
+                    let filename = words[1].to_string();
+                    let rendered = self.render_template(filename);
+                    println!("{}", rendered);
                 },
                 _ => {
                     eprintln!("[literal] unknown directive: {}", line);
